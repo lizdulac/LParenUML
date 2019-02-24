097a0f759bd94468c5cf0abbe0c515961e7790e6
lizdulac <47396187+lizdulac@users.noreply.github.com>
2019-02-23 21:17:45 -0500

    Update GUIcontroller.java

--- gui/GUIcontroller.java
+++ gui/GUIcontroller.java
@@ -1,4 +1,5 @@
 package gui;
+
 import model.*;
 
 import javafx.scene.Node;
@@ -13,141 +14,163 @@ import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 
 public class GUIcontroller
-{	
-	public enum ToolState
-	{
-		SELECT,
-		ADD_NODE,
-		ADD_EDGE
-	};
-
-	private static GUIcontroller sharedController;
-	private GUIview theView;
-	private UGraph theGraph;
-	private ToolState toolState;
-	private int uNodeId;
-	private final ObjectProperty<Point2D> lastClick;
-
-
-	private GUIcontroller(Stage stage)
-	{
-		theGraph = new UGraph();
-		toolState = ToolState.SELECT;
-		uNodeId = 0;
-		lastClick = new SimpleObjectProperty<>();
-
-		System.out.println("\n******* Start *******\n");		
-		theView = new GUIview(this, stage);
-		stage.show();
-	}
-
-	public static GUIcontroller getSharedController(Stage stage)
-	{
-		if(sharedController == null)
-		{
-			sharedController = new GUIcontroller(stage);
-		}
-		return sharedController;
-	}
-
-	public ToolState getToolState()
-	{
-		return toolState;
-	}
-
-	public Integer nextNodeId()
-	{
-		return (Integer) (++uNodeId);
-	}
-
-
-//******************************** EVENT HANDLERS ********************************
-
-	public EventHandler<ActionEvent> buttonClick = new EventHandler<ActionEvent>()
-	{
-		@Override 
-		public void handle(ActionEvent e)
-		{
-			ToolState sourceButton = (ToolState) ((Node) e.getSource()).getUserData();
-
-			switch(sourceButton)
-			{
-				case SELECT:
-					toolState = ToolState.SELECT;
-					break;	
-				case ADD_NODE:
-					toolState = ToolState.ADD_NODE;
-					break;
-				case ADD_EDGE:
-					toolState = ToolState.ADD_EDGE;
-					break;
-				default:
-					toolState = ToolState.SELECT;			
-			}
-			System.out.println("TSTATE: changed to "+toolState);
-		}
-	};
-
-	public EventHandler<MouseEvent> canvasMousePress = new EventHandler<MouseEvent>()
+{
+    public enum ToolState
     {
-        @Override 
-        public void handle(MouseEvent e)
-        {
-        	if(e.getTarget() == e.getSource())
-        	{
-        		if(toolState == ToolState.ADD_NODE)
-        		{
-        			int id = nextNodeId();
-        			String name = ((Character) ((char) (id + 96))).toString();
+        SELECT (0), ADD_NODE (1), ADD_EDGE (2), DELETE (3);
+        
+        private final int value; 
 
-        			theGraph.addNode(id, name);
-        			//theView.drawNode(e.getX(), e.getY(), id);
-
-        			System.out.println("U-NODE: node created -> id="+id+" name="+name);
-        		}
-        		else
-        		{
-        			System.out.println("CANVAS: canvas clicked, no action");
-        		}
-        	}
+        ToolState(int value) { 
+            this.value = value; 
         }
     };
 
-    public EventHandler<MouseEvent> uNodeMousePress = new EventHandler<MouseEvent>()
-    {
-        @Override 
-        public void handle(MouseEvent e)
-        {
-        	double x = e.getSceneX();
-        	double y = e.getSceneY();
-        	Point2D clickPoint = new Point2D(x, y);
-        	lastClick.set(clickPoint);
+    private static GUIcontroller sharedController;
+    private GUIview theView;
+    private UGraph theGraph;
+    private ToolState toolState;
+    private int uNodeId;
+    private final ObjectProperty<Point2D> lastClick;
 
-        	System.out.println("U-NODE: uNode clicked -> x="+x+" y="+y);
+    private GUIcontroller (Stage stage)
+    {
+        theGraph = new UGraph ();
+        toolState = ToolState.SELECT;
+        uNodeId = 0;
+        lastClick = new SimpleObjectProperty<> ();
+
+        System.out.println ("\n******* Start *******\n");
+        theView = new GUIview (this, stage);
+        stage.show ();
+    }
+
+    public static GUIcontroller getSharedController (Stage stage)
+    {
+        if (sharedController == null)
+        {
+            sharedController = new GUIcontroller (stage);
+        }
+        return sharedController;
+    }
+
+    public ToolState getToolState ()
+    {
+        return toolState;
+    }
+
+    public Integer nextNodeId ()
+    {
+        return (Integer) (++uNodeId);
+    }
+
+    // ******************************** EVENT HANDLERS ********************************
+
+    public EventHandler<ActionEvent> buttonClick = new EventHandler<ActionEvent> ()
+    {
+        @Override
+        public void handle (ActionEvent e)
+        {
+            ToolState sourceButton = (ToolState) ((Node) e.getSource ()).getUserData ();
+
+            switch (sourceButton)
+            {
+            case SELECT:
+                toolState = ToolState.SELECT;
+                break;
+            case ADD_NODE:
+                toolState = ToolState.ADD_NODE;
+                break;
+            case ADD_EDGE:
+                toolState = ToolState.ADD_EDGE;
+                break;
+            case DELETE:
+                toolState = ToolState.DELETE;
+                break;
+            default:
+                toolState = ToolState.SELECT;
+            }
+            System.out.println ("TSTATE: changed to " + toolState);
         }
     };
 
-    public EventHandler<MouseEvent> uNodeDrag = new EventHandler<MouseEvent>()
+    public EventHandler<MouseEvent> canvasMousePress = new EventHandler<MouseEvent> ()
     {
-    	@Override 
-        public void handle(MouseEvent e)
+        @Override
+        public void handle (MouseEvent e)
         {
-        	if(toolState == ToolState.SELECT)
-        	{
-        		double x, y;
-        		Rectangle source = ((Rectangle) e.getSource());
-        	
-	        	double offsetX = e.getSceneX() - lastClick.get().getX();
-	        	double offsetY = e.getSceneY() - lastClick.get().getY();
+            if (e.getTarget () == e.getSource ())
+            {
+                if (toolState == ToolState.ADD_NODE)
+                {
+                    int id = nextNodeId ();
+                    String name = ((Character) ((char) (id + 96))).toString ();
 
-	        	source.setX(source.getX() + offsetX);
-	        	source.setY(source.getY() + offsetY);
+                    // theGraph.addNode(id, name);
+                    theView.drawNode (e.getX (), e.getY ());
 
-				Point2D dragPoint = new Point2D(e.getSceneX(), e.getSceneY());
-				lastClick.set(dragPoint);
+                    System.out.println ("U-NODE: node created -> id=" + id + " name=" + name);
+                } else
+                {
+                    System.out.println ("CANVAS: canvas clicked, no action");
+                }
+            }
+        }
+    };
 
-				System.out.println("U-NODE: drag");        		
-        	}
+    public EventHandler<MouseEvent> uNodeMousePress = new EventHandler<MouseEvent> ()
+    {
+        @Override
+        public void handle (MouseEvent e)
+        {
+            double x = e.getSceneX ();
+            double y = e.getSceneY ();
+            Point2D clickPoint = new Point2D (x, y);
+            lastClick.set (clickPoint);
+            
+            Rectangle source = ((Rectangle) e.getSource ());
+            
+            if (toolState == ToolState.DELETE)
+            {
+                theView.deleteNode (source);
+            }
+
+            System.out.println ("U-NODE: uNode clicked -> x=" + x + " y=" + y);
+        }
+    };
+
+    public EventHandler<MouseEvent> uNodeDrag = new EventHandler<MouseEvent> ()
+    {
+        @Override
+        public void handle (MouseEvent e)
+        {
+
+            if (toolState == ToolState.SELECT)
+            {
+                Rectangle source = ((Rectangle) e.getSource ());
+
+                if (lastClick.get () != null)
+                {
+                    double offsetX = e.getSceneX () - lastClick.get ().getX ();
+                    double offsetY = e.getSceneY () - lastClick.get ().getY ();
+
+                    theView.moveNode (source, offsetX, offsetY);
+                    /*
+                     * source.setX(source.getX() + offsetX);
+                     * source.setY(source.getY() + offsetY);
+                     */
+
+                    Point2D dragPoint = new Point2D (e.getSceneX (), e.getSceneY ());
+                    lastClick.set (dragPoint);
+
+                    System.out.println ("U-NODE: drag");
+                }
+
+                double x = e.getSceneX ();
+                double y = e.getSceneY ();
+                Point2D clickPoint = new Point2D (x, y);
+                lastClick.set (clickPoint);
+            }
         }
     };
 }
