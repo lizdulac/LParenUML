149880333d3ac240760361da8c7c12c9e27c4968
An Huynh <38223391+jrtjackal@users.noreply.github.com>
2019-03-29 18:34:36 -0400

    Add files via upload

---
+++ gui/History.java
@@ -0,0 +1,52 @@
+package gui;
+import java.util.*;
+
+
+/**
+ * 
+ * @author Dryver, An
+ *
+ */
+
+/* History is a frame-work class that models the History for REDO UNDO 
+ * but a lot more simplistic. 
+ * It is purely to save executed commands and to undo/redo them
+ */
+
+
+public final class History {
+	private static History instance = null;
+	private final stack<Command> undoStack = new Stack<Command>();
+	
+	public void execute (final Command cmd)
+	{
+		undoStack.push(cmd);
+		cmd.execute();
+	}
+	
+	public void undo()
+	{
+		if (!undoStack.isEmpty())
+		{
+			Command cmd = undoStack.pop();
+			cmd.undo();
+		} else {
+			System.out.println ("Nothing to undo here");
+		}
+	}
+	public static History getInstance()
+	{
+		if ( History.instance == null) {
+			sychronized (History.class) {
+				if ( History.instance == null ) {
+					History.instance = new History ();
+				}
+			}
+		}
+		return History.instance;
+	}
+	private History() {}
+}
+
+
+
