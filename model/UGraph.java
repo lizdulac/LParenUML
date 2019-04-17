78c75366bce65266c69fd1afc62bb73214d30fee
lizdulac <47396187+lizdulac@users.noreply.github.com>
2019-04-17 00:21:50 -0400

    Add files via upload

--- model/UGraph.java
+++ model/UGraph.java
@@ -9,47 +9,67 @@ import java.util.*;
  * 
 */
 public class UGraph {
-	
-	public Map< Integer ,UNode> uNodes;
-	
-	public UGraph()
-	{
-		uNodes = new HashMap<Integer , UNode>();	
-	}
-	
-	
-	public UNode getNode(Integer nodeid)
-	{
-		return uNodes.get(nodeid);
-	}
-	
-	public boolean addNode(Integer id, String nodeName)
-	{
-		if (uNodes.put( id, new UNode( id, nodeName)) != null) 
-			return true;
-		return false;
-	}
-	
-	
-	/*
-	 * Inbound Iteration 2 
-	 */
-	public void removeNode (String id)
-	{
-		uNodes.get(id).cleanEdges();
-		uNodes.remove(id); //hard remove
-	}
-	
-	
-	
-	/*
-	 * n1 would be the click node and n2 would be the release node
-	 */
-	public void linkSingle(UNode n1, UNode n2, String edge)
-	{
-		Edge e = new Edge(n1, n2);
-		n1.addOutEdge(e);
-		n2.addInEdge(e);
-	}
+    
+    // Map of Nodes
+    public Map< Integer ,UNode> uNodes;
+    
+       /*
+    * Empty Constructor a new UGraph. Inintializes an empty Map to store unique nodes in.
+       */
+    public UGraph()
+    {
+        uNodes = new HashMap<Integer , UNode>();
+    }
+        
+    
+    public boolean addNode(Integer id, String nodeName)
+    {
+        if (uNodes.put( id, new UNode( id, nodeName)) != null) 
+            return true;
+        return false;
+    }
+    
+    /*
+     * Searches the map based on the key given
+     * Parameters: Key
+    */
+    public UNode getNode(Integer nodeid)
+    {
+        return uNodes.get(nodeid);
+    }
+    
+
+    
+    
+    /**
+     * Inbound Iteration 2 
+     */
+    public void removeNode (String id)
+    {
+        uNodes.get(id).cleanEdges();
+        uNodes.remove(id); //hard remove
+    }
+    
+    
+    
+    /*
+     * This Method links two nodes with a single UEdge
+     * Parameters -
+     */
+    public void linkSingle(UNode n1, UNode n2, String edge)
+    {
+        UEdge e = new UEdge(n1, n2);
+        n1.addOutEdge(e);
+        n2.addInEdge(e);
+    }
+    
+    public Integer[] getAllNodes ()
+    {
+        Object[] temp = uNodes.keySet ().toArray ();
+        Integer[] keys = new Integer[temp.length];
+        System.arraycopy (temp, 0, keys, 0, temp.length);
+        Arrays.sort (keys);
+        return keys;
+    }
 
 }
